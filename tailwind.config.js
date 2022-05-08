e42d83c9cc38cabea4e90f00f28c67ef5902b42b
Piyush Aggarwal <27003616+ThePiyushAggarwal@users.noreply.github.com>
2022-05-08 12:20:20 +0530

    mid-commit. font-family works without escaping
    tailwind docs mention it won't work but it does work

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,20 @@ module.exports = {
     screens: {
       sm: '375px',
     },
-    extend: {},
+
+    extend: {
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],
+        lexend: ['Lexend Deca', 'sans-serif'],
+      },
+      colors: {
+        veryDarkBlue: 'hsl(233, 47%, 7%)',
+        darkDesaturatedBlue: 'hsl(244, 38%, 16%)',
+        softViolet: 'hsl(277, 64%, 61%)',
+        paragraph: 'hsla(0, 0%, 100%, 0.75)',
+        statHeading: 'hsla(0, 0%, 100%, 0.6)',
+      },
+    },
   },
   plugins: [],
 }
